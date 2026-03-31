5d6ae072c31a63a752d33a32c9cf45cad46023ad
Gabriel Hess <heg2@bfh.ch>
2026-03-31 08:22:24 +0200

    clearer instructions

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import { JSOnFhir } from "@i4mi/js-on-fhir";
 
-// connect to the relevant elements of the DOM
+// connect to the relevant elements of the HTML DOM
 const patientNameSpan = document.getElementById('patient-name-span');
 const familyNameInput = document.getElementById('name-input');
 const loadButton = document.getElementById('load-button');
 loadButton.addEventListener('click', buttonClickHandler);
 
 
-// set up js-on-fhir library TODO: enter correct value(s) for the medinf lab server
+/* 
+    set up js-on-fhir library 
+    TODO: enter correct value(s) for the medinf lab server
+    see js-on-fhir documentation: https://github.com/i4mi/fhir-wrappers.ts/tree/master/web
+*/
 const jsOnFhir = new JSOnFhir(
     'https//???',               // the BASE URL of the server
     '',                         // the client id  - only needed for SMART on FHIR flow (not necessary with medinf lab server)
@@ -21,10 +25,12 @@ const jsOnFhir = new JSOnFhir(
 async function buttonClickHandler() {
     /*
         TODO:
-            - use jsOnFhir to make a request for loading your patient resource 
+            - use jsOnFhir for searching a patient resource that matches the value of familyNameInput
             - log the result to the console
             - extract the patient given name from the loaded resource
-            - set the name to the patientNameSpan
+            - set the given name to the patientNameSpan
+
+        see js-on-fhir documentation: https://github.com/i4mi/fhir-wrappers.ts/tree/master/web
     */
     patientNameSpan.innerText = familyNameInput.value;
 }
